1e942704b711878ee77a01f756d04b6b2037b644
MI <minhazulislam@MI.local>
2024-09-15 21:42:23 +0100

    aligment made for all sections

--- index2.js
+++ index2.js
@@ -1,3 +1,31 @@
+let smallerSlider = document.querySelector("#smallerSlider");
+
+let RPS = document.createElement("img");
+RPS.setAttribute('src', './assets/images/RPS.png');
+RPS.classList.add("slides")
+smallerSlider.appendChild(RPS)
+
+let WDS = document.createElement("img");
+WDS.setAttribute("src", "./assets/images/WDS.png");
+WDS.classList.add("slides")
+smallerSlider.appendChild(WDS)
+
+let weather = document.createElement("img");
+weather.setAttribute("src", "./assets/images/weather.png");
+weather.classList.add("slides")
+smallerSlider.appendChild(weather)
+
+let reactProfile = document.createElement("img");
+reactProfile.setAttribute("src", "./assets/images/profile.png");
+reactProfile.classList.add("slides")
+smallerSlider.appendChild(reactProfile)
+
+let TDL = document.createElement("img");
+TDL.setAttribute("src", "./assets/images/tdl.png");
+TDL.classList.add("slides")
+smallerSlider.appendChild(TDL)
+
+
 let index = 1;
 
 const plusIndex = (n) => {
@@ -23,5 +51,3 @@ const showImage = (n) => {
 
   x[index - 1].style.display = "block";
 };
-
-show(1);
