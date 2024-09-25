4a9a3c2faadb5089dc4c26251e5c4aed1ad215e6
MI <minhazulislam@MI.local>
2024-09-25 10:18:16 +0100

    nav bar width 100% and work carusel buttons change color on switch

--- nav.js
+++ nav.js
@@ -2,19 +2,38 @@ const nav = document.querySelector("#nav");
 const checkbox = document.querySelector(".checkbox");
 const checkbox2 = document.querySelector(".checkbox2");
 const star = document.querySelector("*");
+const GCP = document.querySelector(".gallery-controls-previous");
+const GCN = document.querySelector(".gallery-controls-next")
 
 const background = () => {
-  star.style.background = "#282828";
-  star.style.color = "white";
+  let dominant = window.getComputedStyle(document.documentElement).getPropertyValue("--dominant");
+  let minor = window.getComputedStyle(document.documentElement).getPropertyValue("--minor");
+  let dominant2 = window.getComputedStyle(document.documentElement).getPropertyValue("--dominant2");
+  let minor2 = window.getComputedStyle(document.documentElement).getPropertyValue("--minor2");
+
+  star.style.background = dominant
+  star.style.color = minor
+  GCP.style.color = minor
+  GCN.style.color = minor
+      
+  
   // function for day and night mode
 
   checkbox.addEventListener("change", () => {
     if (checkbox.checked) {
-      star.style.background = "#d7d0d0";
-      star.style.color = "#282828";
+      star.style.background = dominant2;
+      star.style.color = minor2;
+      GCP.style.color = minor2
+      GCN.style.color = minor2
+  
+      
     } else {
-      star.style.background = "#282828";
-      star.style.color = "white";
+      star.style.background = dominant;
+      star.style.color = minor;
+      GCP.style.color = minor
+      GCN.style.color = minor
+      infoDisplay.style.color = minor
+      
     }
   });
 
